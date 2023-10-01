20162cee5221bb3bb4f9aabf30c126a45029000b
Hannah Mills <millsh56@gmail.com>
2023-10-01 13:01:53 -0400

    commit for establishing music library project

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,11 @@
+import GalleryItem from './GalleryItem'
+
+function Gallery (props) {
+    return (
+        <div style={{'width': '100%'}}>
+            <GalleryItem/>
+        </div>
+    )
+}
+
+export default Gallery
---
+++ src/components/GalleryItem.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function GalleryItem(props) {
+
+    let [view, setView] = useState(false)
+
+    return(
+        <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
+            <p>Just One Gallery Item!</p>
+        </div>
+    )
+}
+
+export default GalleryItem
